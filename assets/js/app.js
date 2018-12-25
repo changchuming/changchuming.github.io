a275c7c1d849199299e35425a41c60067cb73b35
Chang Chu-Ming <changchuming@gmail.com>
2018-12-24 19:26:07 -0800

    Fixed popup js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,5 @@
 $( document ).ready(function() {
 
-	alert("asdf");
-	$('.thumbnail img').each(function() {
-		var currentImage = $(this);
-		currentImage.wrap("<a class='image-link' href='" + currentImage.attr("src") + "'</a>");
-	});
-	$('.image-link').magnificPopup({type:'image'});  
-
 	/* Sidebar height set */
 	$('.sidebar').css('min-height',$(document).height());
 
@@ -19,5 +12,11 @@ $( document ).ready(function() {
 	contact_list.mouseenter(function(){ scontacts.fadeIn(); });
 	
 	contact_list.mouseleave(function(){ scontacts.fadeOut(); });
+
+	$('.thumbnail').each(function() {
+		var currentImage = $(this);
+		currentImage.wrap("<a class='image-link' href='" + currentImage.attr("src") + "'</a>");
+	});
+	$('.image-link').magnificPopup({type:'image'});  
 
 });
